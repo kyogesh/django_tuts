a9f3dfda75850387fb264c2cf43e78dc317af7c8
Yogesh Kumar <yk.301189@gmail.com>
2014-11-10 19:08:18 +0530

    Lot more changes according to pep8 convention.

--- review/review/urls.py
+++ review/review/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'review.views.home', name='home'),
-                       url(r'^myapp/', include('myapp.urls', namespace='myapp')),
-                       url(r'^forms/', include('formtut.urls', namespace='forms')),
-                       url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns\
+('',
+# Examples:
+# url(r'^$', 'review.views.home', name='home'),
+url(r'^myapp/', include('myapp.urls', namespace='myapp')),
+url(r'^forms/', include('formtut.urls', namespace='forms')),
+url(r'^admin/', include(admin.site.urls)),
 )
