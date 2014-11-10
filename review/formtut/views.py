278bd414595a32af9920c44eef592060cde2f0f6
Yogesh Kumar <yk.301189@gmail.com>
2014-11-10 18:58:10 +0530

    Lot more changes according to pep8 convention.

--- review/formtut/views.py
+++ review/formtut/views.py
@@ -21,10 +21,10 @@ def edit(request, person_id):
     initial_data = {
                    'name': person.name,
                    'birth_place': person.birth_place,
-                   'birth_date': person.birth_date,
-                   }
+                   'birth_date': person.birth_date, }
     personform = PersonForm(initial=initial_data)
-    return render(request, 'formtut/edit.html', {'person': person, 'personform': personform})
+    return render(request, 'formtut/edit.html',
+                 {'person': person, 'personform': personform})
 
 
 def save(request, person_id):
@@ -36,5 +36,5 @@ def save(request, person_id):
         personform.save()
     else:
         print 'Invalid Data'
-   
+
     return HttpResponseRedirect(reverse('forms:detail', args=(person.id, )))
