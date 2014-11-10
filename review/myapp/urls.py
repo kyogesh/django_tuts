351878920427fb0040ce7b510ad54f3748f7b2b5
Yogesh Kumar <yk.301189@gmail.com>
2014-11-10 19:13:02 +0530

    Lot more changes according to pep8 convention.

--- review/myapp/urls.py
+++ review/myapp/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import patterns, include, url
 
 from . import views
 
-urlpatterns = patterns\
-('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+                       url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+                       url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+                       url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
